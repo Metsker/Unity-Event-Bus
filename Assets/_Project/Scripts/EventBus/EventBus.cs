9e14cefefe0a6c0a113f0195dd361de02bf5e4ca
Lev <66064249+Metsker@users.noreply.github.com>
2025-03-14 16:24:30 +0300

    Update EventBus.cs

--- Assets/_Project/Scripts/EventBus/EventBus.cs
+++ Assets/_Project/Scripts/EventBus/EventBus.cs
@@ -1,25 +1,33 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 
-public static class EventBus<T> where T : IEvent {
-    static readonly HashSet<IEventBinding<T>> bindings = new HashSet<IEventBinding<T>>();
-    
-    public static void Register(EventBinding<T> binding) => bindings.Add(binding);
-    public static void Deregister(EventBinding<T> binding) => bindings.Remove(binding);
+public static class EventBus<T> where T : struct, IEvent
+{
+    private static readonly HashSet<EventBinding<T>> Bindings = new ();
 
-    public static void Raise(T @event) {
-        var snapshot = new HashSet<IEventBinding<T>>(bindings);
+    public static void Register(Action<T> callback) => Bindings.Add((EventBinding<T>)callback);
+    public static void Register(Action callbackNoArgs) => Bindings.Add((EventBinding<T>)callbackNoArgs);
+    public static void Deregister(Action<T> callback) => Bindings.Remove((EventBinding<T>)callback);
+    public static void Deregister(Action callbackNoArgs) => Bindings.Remove((EventBinding<T>)callbackNoArgs);
 
-        foreach (var binding in snapshot) {
-            if (bindings.Contains(binding)) {
-                binding.OnEvent.Invoke(@event);
-                binding.OnEventNoArgs.Invoke();
-            }
+    public static void Raise(T @event)
+    {
+        HashSet<EventBinding<T>> snapshot = new (Bindings);
+
+        foreach (EventBinding<T> binding in snapshot.Where(binding => Bindings.Contains(binding)))
+        {
+            binding.OnEvent.Invoke(@event);
+            binding.OnEventNoArgs.Invoke();
         }
     }
 
-    static void Clear() {
+    [UsedImplicitly]
+    private static void Clear()
+    {
         Debug.Log($"Clearing {typeof(T).Name} bindings");
-        bindings.Clear();
+        Bindings.Clear();
     }
 }
